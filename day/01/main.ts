0a60c95c7f08fee87ddb2f3ebd2ba3107d91e14f
adminadmin <git@akki.uk>
2025-12-01 14:40:13 +0000

    tidy up

--- day/01/main.ts
+++ day/01/main.ts
@@ -2,10 +2,7 @@ import { parseArgs } from '@/lib/args.0.ts';
 import { Logger } from '@/lib/logger.0.ts';
 import { Utils } from '@/lib/utils.0.ts';
 
-function part1(data: string, logger: Logger) {
-  const [names, moves] = data.split('\n\n').map((line) => line.split(','));
-  const operations = moves.map((token) => parseInt(token.slice(1)) * (token.slice(0, 1) === 'L' ? -1 : 1));
-  logger.debugLow(names, operations);
+function part1(names: string[], operations: number[], logger: Logger) {
   let i = 0;
   for (const operation of operations) {
     i = Utils.clamp(i + operation, 0, names.length - 1);
@@ -15,10 +12,7 @@ function part1(data: string, logger: Logger) {
   logger.success(names[i]);
 }
 
-function part2(data: string, logger: Logger) {
-  const [names, moves] = data.split('\n\n').map((line) => line.split(','));
-  const operations = moves.map((token) => parseInt(token.slice(1)) * (token.slice(0, 1) === 'L' ? -1 : 1));
-  logger.debugLow(names, operations);
+function part2(names: string[], operations: number[], logger: Logger) {
   let i = 0;
   for (const operation of operations) {
     i = Utils.modP(i + operation, names.length);
@@ -28,10 +22,7 @@ function part2(data: string, logger: Logger) {
   logger.success(names[i]);
 }
 
-function part3(data: string, logger: Logger) {
-  const [names, moves] = data.split('\n\n').map((line) => line.split(','));
-  const operations = moves.map((token) => parseInt(token.slice(1)) * (token.slice(0, 1) === 'L' ? -1 : 1));
-  logger.debugLow(names, operations);
+function part3(names: string[], operations: number[], logger: Logger) {
   for (const operation of operations) {
     const i = Utils.modP(operation, names.length);
     logger.debugLow({ operation, i }, names[0], names[i]);
@@ -43,9 +34,12 @@ function part3(data: string, logger: Logger) {
 
 function main() {
   const { data, logger, part } = parseArgs(import.meta.url);
-  if (part === 1) part1(data, logger.makeChild('part1'));
-  if (part === 2) part2(data, logger.makeChild('part2'));
-  if (part === 3) part3(data, logger.makeChild('part3'));
+  const [names, moves] = data.split('\n\n').map((line) => line.split(','));
+  const operations = moves.map((token) => parseInt(token.slice(1)) * (token.slice(0, 1) === 'L' ? -1 : 1));
+  logger.debugLow(names, operations);
+  if (part === 1) part1(names, operations, logger.makeChild('part1'));
+  if (part === 2) part2(names, operations, logger.makeChild('part2'));
+  if (part === 3) part3(names, operations, logger.makeChild('part3'));
 }
 
 main();
--- day/02/main.ts
+++ day/02/main.ts
@@ -1,104 +1,124 @@
 import { parseArgs } from '@/lib/args.0.ts';
 import { Logger } from '@/lib/logger.0.ts';
+import { inspect } from 'node:util';
 
-type Complex = [x: number, y: number];
+type ComplexParams = [complex: Complex] | [x: number, y: number];
 
-function add(a: Complex, b: Complex): Complex {
-  return [a[0] + b[0], a[1] + b[1]];
-}
-
-function mult(a: Complex, b: Complex): Complex {
-  return [
-    a[0] * b[0] - a[1] * b[1],
-    a[0] * b[1] + a[1] * b[0],
-  ];
-}
-
-function div(a: Complex, b: Complex): Complex {
-  return [
-    Math.trunc(a[0] / b[0]),
-    Math.trunc(a[1] / b[1]),
-  ];
-}
-
-function part1(input: Complex, logger: Logger) {
-  let result: Complex = [0, 0];
-  for (let i = 0; i < 3; ++i) {
-    result = mult(result, result);
-    logger.debugLow(i, 'mult', result);
-    result = div(result, [10, 10]);
-    logger.debugLow(i, 'div', result);
-    result = add(result, input);
-    logger.debugLow(i, 'add', result);
+class Complex {
+  #x: number;
+  #y: number;
+  constructor(...[p0, p1]: ComplexParams) {
+    if (p0 instanceof Complex) {
+      this.#x = p0.x;
+      this.#y = p0.y;
+    } else if (typeof p1 === 'number') {
+      this.#x = p0;
+      this.#y = p1;
+    } else { throw new Error('bad constructor params'); }
   }
-  // [125233,708620]
-  logger.success('result', `[${result[0]},${result[1]}]`);
+  public get x() {
+    return this.#x;
+  }
+  public get y() {
+    return this.#y;
+  }
+  /** mutate in-place */
+  add(...[p0, p1]: ComplexParams): this {
+    if (p0 instanceof Complex) {
+      this.#x += p0.x;
+      this.#y += p0.y;
+    } else if (typeof p1 === 'number') {
+      this.#x += p0;
+      this.#y += p1;
+    }
+    return this;
+  }
+  /** mutate in-place */
+  mult(...[p0, p1]: ComplexParams): this {
+    if (p0 instanceof Complex) {
+      const x = this.x * p0.x - this.y * p0.y;
+      const y = this.x * p0.y + this.y * p0.x;
+      this.#x = x;
+      this.#y = y;
+    } else if (typeof p1 === 'number') {
+      const x = this.x * p0 - this.y * p1;
+      const y = this.x * p1 + this.y * p0;
+      this.#x = x;
+      this.#y = y;
+    }
+    return this;
+  }
+  /** mutate in-place */
+  div(...[p0, p1]: ComplexParams): this {
+    if (p0 instanceof Complex) {
+      this.#x = Math.trunc(this.x / p0.x);
+      this.#y = Math.trunc(this.y / p0.y);
+    } else if (typeof p1 === 'number') {
+      this.#x = Math.trunc(this.x / p0);
+      this.#y = Math.trunc(this.y / p1);
+    }
+    return this;
+  }
+  [inspect.custom]() {
+    return `[${this.x},${this.y}]`;
+  }
 }
 
-function part2(input: Complex, logger: Logger) {
-  const resolution = 101;
-  const size = 1000;
+function getEngraveCount(input: Complex, size: number, resolution: number, logger: Logger): number {
   let engraveCount = 0;
   for (let y = 0; y < resolution; ++y) {
     for (let x = 0; x < resolution; ++x) {
-      const point = add(input, [x * size / (resolution - 1), y * size / (resolution - 1)]);
-      let result: Complex = [0, 0];
+      const point = new Complex(input).add(x * size / (resolution - 1), y * size / (resolution - 1));
+      const result = new Complex(0, 0);
+      let shouldEngrave = true;
 
-      for (let i = 0; i < 100; ++i) {
-        result = mult(result, result);
-        result = div(result, [100000, 100000]);
-        result = add(result, point);
-        // logger.debugMed({ x, y, point, i, result });
+      for (let i = 0; shouldEngrave && i < 100; ++i) {
+        result.mult(result);
+        result.div(100000, 100000);
+        result.add(point);
         if (
-          result[0] < -1000000 || result[0] > 1000000 || result[1] < -1000000 && result[1] > 1000000
-        ) { break; }
+          result.x < -1000000 || result.x > 1000000 || result.y < -1000000 || result.y > 1000000
+        ) { shouldEngrave = false; }
       }
 
-      const shouldEngrave = result[0] >= -1000000 && result[0] <= 1000000 && result[1] >= -1000000 && result[1] <= 1000000;
       if (shouldEngrave) ++engraveCount;
       logger.debugLow({ x, y, result, shouldEngrave });
     }
   }
+  return engraveCount;
+}
+
+function part1(input: Complex, logger: Logger) {
+  const value = new Complex(0, 0);
+  for (let i = 0; i < 3; ++i) {
+    value.mult(value);
+    logger.debugLow(i, 'mult', value);
+    value.div(10, 10);
+    logger.debugLow(i, 'div', value);
+    value.add(input);
+    logger.debugLow(i, 'add', value);
+  }
+  // [125233,708620]
+  logger.success('result', value);
+}
+
+function part2(input: Complex, logger: Logger) {
+  const engraveCount = getEngraveCount(input, 1000, 101, logger);
   // 1584
   logger.success('engraveCount', engraveCount);
 }
 
 function part3(input: Complex, logger: Logger) {
-  const resolution = 1001;
-  const size = 1000;
-  let engraveCount = 0;
-  for (let y = 0; y < resolution; ++y) {
-    for (let x = 0; x < resolution; ++x) {
-      const point = add(input, [x * size / (resolution - 1), y * size / (resolution - 1)]);
-      let result: Complex = [0, 0];
-
-      for (let i = 0; i < 100; ++i) {
-        result = mult(result, result);
-        result = div(result, [100000, 100000]);
-        result = add(result, point);
-        // logger.debugMed({ x, y, point, i, result });
-        if (
-          result[0] < -1000000 || result[0] > 1000000 || result[1] < -1000000 && result[1] > 1000000
-        ) { break; }
-      }
-
-      const shouldEngrave = result[0] >= -1000000 && result[0] <= 1000000 && result[1] >= -1000000 && result[1] <= 1000000;
-      if (shouldEngrave) ++engraveCount;
-      logger.debugLow({ x, y, result, shouldEngrave });
-    }
-  }
+  const engraveCount = getEngraveCount(input, 1000, 1001, logger);
   // 155348
   logger.success('engraveCount', engraveCount);
 }
 
 function main() {
   const { data, logger, part } = parseArgs(import.meta.url);
-  const match = /A=\[(-?\d+),(-?\d+)\]/.exec(data);
-  if (!match) throw new Error('could not parse input');
-  const input: Complex = [
-    parseInt(match[1]),
-    parseInt(match[2]),
-  ];
+  const match = /A=\[(?<x>-?\d+),(?<y>-?\d+)\]/.exec(data);
+  if (!match?.groups) throw new Error('could not parse input');
+  const input = new Complex(parseInt(match.groups.x), parseInt(match.groups.y));
   logger.debugLow({ data, input });
   if (part === 1) part1(input, logger.makeChild('part1'));
   if (part === 2) part2(input, logger.makeChild('part2'));
