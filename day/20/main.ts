8bf92da43b3d8ec13792a6191e737166f90977d2
adminadmin <git@akki.uk>
2025-12-16 18:11:35 +0000

    day20 minor refactor

--- day/20/main.ts
+++ day/20/main.ts
@@ -1,11 +1,14 @@
 import { EcArgParser } from '@/lib/args.1.ts';
-import { DefaultMap } from '@/lib/default-map.0.ts';
 import { Deque } from '@/lib/deque.0.ts';
 import { CoordSystem, Grid } from '@/lib/grid.0.ts';
 import { Logger } from '@/lib/logger.0.ts';
 import { PackedSet } from '@/lib/packed-set.0.ts';
 import { Offset2D, Point2D, Point2DLike } from '@/lib/point2d.0.ts';
 import { Utils } from '@/lib/utils.0.ts';
+
+interface QueueItem extends Point2DLike {
+  jumps: number;
+}
 
 function part1(data: string, logger: Logger) {
   const grid = new Grid(data.split('\n').map((line) => line.split('')), CoordSystem.Xy);
@@ -18,6 +21,7 @@ function part1(data: string, logger: Logger) {
   let pairs = 0;
   for (const cell of grid.findAll((value) => value === 'T')) {
     for (const offset of offsets) {
+      // exclude vertical moves over corners
       if (offset.y === 1 && Utils.modP(centerX - cell.x, 2) !== cell.y % 2) continue;
       const neighbour = Point2D.add(cell, offset);
       if (grid.inBounds(neighbour) && grid.cellAt(neighbour) === 'T') {
@@ -34,25 +38,24 @@ function part1(data: string, logger: Logger) {
 function part2(data: string, logger: Logger) {
   const grid = new Grid(data.split('\n').map((line) => line.split('')), CoordSystem.Xy);
   const start = grid.find((value) => value === 'S');
-  const end = grid.find((value) => value === 'E');
-  logger.debugLow({ start, end });
-  if (!start || !end) throw new Error('oh no');
+  logger.debugLow({ start });
+  if (!start) throw new Error('oh no');
+
   const seen = new PackedSet(Point2D.pack32, Point2D.unpack32, [start]);
-  interface QueueItem extends Point2DLike {
-    jumps: number;
-  }
   const queue = new Deque<QueueItem>([{ x: start.x, y: start.y, jumps: 0 }]);
   const offsets = Point2D.offsets(1, Offset2D.Cardinal);
   let best = Infinity;
   const centerX = Math.floor(grid.cols / 2);
+
   while (queue.size && best === Infinity) {
     const item = queue.popFront();
     if (!item) throw new Error('oh no');
     for (const offset of offsets) {
       if (offset.y !== 0) {
-        const idk = Utils.modP(centerX - item.x, 2);
-        if (offset.y === -1 && idk === item.y % 2) continue;
-        if (offset.y === 1 && idk !== item.y % 2) continue;
+        // exclude vertical moves over corners
+        const centerXDistMod2 = Utils.modP(centerX - item.x, 2);
+        if (offset.y === -1 && centerXDistMod2 === item.y % 2) continue;
+        if (offset.y === 1 && centerXDistMod2 !== item.y % 2) continue;
       }
       const nextItem = { ...Point2D.add(item, offset), jumps: item.jumps + 1 };
       if (!grid.inBounds(nextItem)) continue;
@@ -73,7 +76,7 @@ function part2(data: string, logger: Logger) {
 }
 
 function part3(data: string, logger: Logger) {
-  // pushing everything over to the left makes point translation easier
+  // pushing everything over to the left makes point rotation easier
   const grid = new Grid(
     data.split('\n').map((line) => {
       const width = line.length;
@@ -83,15 +86,14 @@ function part3(data: string, logger: Logger) {
     CoordSystem.Xy,
   );
   const start = grid.find((value) => value === 'S');
-  const end = grid.find((value) => value === 'E');
-  logger.debugLow({ start, end });
-  if (!start || !end) throw new Error('oh no');
+  logger.debugLow({ start });
+  if (!start) throw new Error('oh no');
 
-  function rotate(point: Point2DLike, value: number): Point2DLike {
-    let current = { ...point };
+  function rotate(item: QueueItem): Point2DLike {
+    let current: Point2DLike = { ...item };
     let next: Point2DLike;
-    for (let i = 0; i < value; ++i) {
-      // i just played with the numbers until the translated values matched my test file
+    for (let i = 0; i < item.jumps % 3; ++i) {
+      // i just played with the numbers until the rotated values matched my test file
       next = {
         x: grid.cols - 1 - current.y * 2 + current.x % 2,
         y: grid.rows - 1 - current.y + Math.ceil(current.x / 2),
@@ -101,15 +103,15 @@ function part3(data: string, logger: Logger) {
     return current;
   }
 
-  // first level index is jumps % 3
-  const seen = new DefaultMap<number, PackedSet<Point2DLike, number>>(() => new PackedSet(Point2D.pack32, Point2D.unpack32));
-  seen.get(0).add(start);
-
-  interface QueueItem extends Point2DLike {
-    jumps: number;
+  /** for generating unique keys for the `seen` set\
+   * `jumps % 3` is included to account for rotation states */
+  function pack(item: QueueItem): number {
+    return item.x | (item.y << 8) | ((item.jumps % 3) << 16);
   }
-  const queue = new Deque<QueueItem>([{ x: start.x, y: start.y, jumps: 0 }]);
 
+  const first = { x: start.x, y: start.y, jumps: 0 };
+  const seen = new PackedSet(pack, undefined, [first]);
+  const queue = new Deque<QueueItem>([first]);
   let best = Infinity;
 
   while (queue.size && best === Infinity) {
@@ -128,16 +130,19 @@ function part3(data: string, logger: Logger) {
     ) {
       const nextItem = { ...Point2D.add(item, offset), jumps: item.jumps + 1 };
       if (!grid.inBounds(nextItem)) continue;
-      // only need to translate to get the cell value
-      const value = grid.cellAt(rotate(nextItem, nextItem.jumps % 3));
-      if (value === '#' || value === '.') continue;
-      if (value === 'E') {
-        logger.debugLow('finished', nextItem);
-        best = nextItem.jumps;
-        break;
+      // only need the cell value in rotated coordinate space
+      switch (grid.cellAt(rotate(nextItem))) {
+        case '.':
+        case '#':
+          continue;
+        case 'E': {
+          logger.debugLow('finished', nextItem);
+          best = nextItem.jumps;
+          break;
+        }
       }
-      if (seen.get(nextItem.jumps % 3).has(nextItem)) continue;
-      seen.get(nextItem.jumps % 3).add(nextItem);
+      if (seen.has(nextItem)) continue;
+      seen.add(nextItem);
       queue.pushBack(nextItem);
     }
   }
